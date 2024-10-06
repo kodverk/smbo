d19575cfba4fbdbdff4535cf9b4f0136141534cc
viktor malmedal <viktor@malmedal.dev>
2024-10-06 22:24:32 +0200

    update

--- infra/db.ts
+++ infra/db.ts
@@ -1,10 +1,8 @@
-export const d1 = new sst.cloudflare.D1("D1");
+export const d1 = new sst.cloudflare.D1("D1Database");
 
-export const database = new sst.Linkable("Database", {
+export const database = new sst.Linkable("D1Properties", {
   properties: {
     id: d1.id,
     accountId: d1.nodes.database.accountId,
   },
 });
-
-export const d1Token = new sst.Secret("D1_TOKEN");
---
+++ infra/secrets.ts
@@ -0,0 +1,3 @@
+export const secrets = {
+  d1Token: new sst.Secret("D1_TOKEN"),
+};
