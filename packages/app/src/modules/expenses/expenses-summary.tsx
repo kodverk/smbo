5ab0a8c3c1de15a1a39bc36bb5b47408897cb3de
viktor malmedal <viktor@malmedal.dev>
2024-10-22 08:47:13 +0200

    add expense list

--- packages/app/src/modules/expenses/expenses-summary.tsx
+++ packages/app/src/modules/expenses/expenses-summary.tsx
@@ -1,33 +1,86 @@
+import { formatRelative } from "date-fns";
 import React from "react";
-import { Text, View } from "react-native";
-import { HomeStore } from "../home/home.store";
+import { FlatList, Text, View } from "react-native";
+import { trpc, type RouterOutputs } from "~/trpc/provider";
 
 export function ExpenseSummary() {
-  const id = HomeStore.useSelectedId();
   return (
     <View className="bg-neutral-900 border border-neutral-800 rounded-lg py-3 px-4 gap-y-8">
-      <View className="gap-y-2">
-        <Text className="text-neutral-200 text-md">You are owed:</Text>
-        <Text className="text-3xl font-bold text-green-500">+ 5600 kr</Text>
-      </View>
-      {new Array(3).fill(null).map((_, i) => (
-        <ExpenseContent key={i} />
-      ))}
+      <Owed />
+      <ExpenseList />
       <Text className="text-emerald-400 font-medium text-lg text-center">See all</Text>
     </View>
   );
 }
 
-function ExpenseContent() {
+function ExpenseList() {
+  const transactions = trpc.transactions.many.useQuery({
+    offset: 0,
+    limit: 10,
+  });
+  return (
+    <FlatList
+      data={transactions.data}
+      renderItem={({ item }) => <ExpenseItem transaction={item} />}
+    />
+  );
+}
+
+function Owed() {
+  const owed = trpc.transactions.totalOwed.useQuery();
+
+  if (owed.isLoading) {
+    return <Text>Loading...</Text>;
+  }
+
+  if (owed.error) {
+    return <Text>Error</Text>;
+  }
+
+  if (owed.data.totalOwed > owed.data.totalOwedToUser) {
+    return (
+      <View className="gap-y-2">
+        <Text className="text-neutral-200 text-md">You are owed:</Text>
+        <Text className="text-3xl font-bold text-green-500">+ 5600 kr</Text>
+      </View>
+    );
+  }
+
+  if (owed.data.totalOwed < owed.data.totalOwedToUser) {
+    return (
+      <View className="gap-y-2">
+        <Text className="text-neutral-200 text-md">You owe:</Text>
+        <Text className="text-3xl font-bold text-red-500">- 5600 kr</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View className="gap-y-2">
+      <Text className="text-neutral-200 text-md">Score is settled:</Text>
+      <Text className="text-3xl font-bold text-neutral-200">+ 0 kr</Text>
+    </View>
+  );
+}
+
+interface ExpenseItemProps {
+  transaction: RouterOutputs["transactions"]["many"][number];
+}
+
+function ExpenseItem(props: ExpenseItemProps) {
   return (
     <View className="flex-row gap-x-4 items-center">
       <View className="rounded-3xl h-8 w-8 bg-orange-300" />
       <View className="flex-1">
         <View className="flex-row justify-between items-end">
-          <Text className="text-gray-300 font-medium text-lg">Hawaii Poke Sveavagen</Text>
-          <Text className="text-gray-200 text-lg">+200 kr</Text>
+          <Text className="text-gray-300 font-medium text-lg">{props.transaction.amount}</Text>
+          <Text className="text-gray-200 text-lg">
+            +{props.transaction.amount} {props.transaction.currency}
+          </Text>
         </View>
-        <Text className="text-gray-400">Yesterday, 12:17</Text>
+        <Text className="text-gray-400">
+          {formatRelative(props.transaction.transactionDate, new Date())}
+        </Text>
       </View>
     </View>
   );
